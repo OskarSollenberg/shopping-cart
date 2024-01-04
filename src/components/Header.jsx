8043ce89250b8402235acee601cc58091a6c2d89
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-01-04 19:33:57 +0100

    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import Cart from "./Cart";
+
+function Header(props) {
+    const { cartItems } = props;
+
+    return (
+        <div
+            style={{
+                backgroundColor: "#282c34",
+                minHeight: 60,
+                paddingLeft: 10,
+                paddingRight: 10,
+                paddingTop: 10,
+                display: "flex",
+                fontSize: "2em",
+                color: "white",
+            }}
+        >
+            <div>My Shop</div>
+            <Cart numItems={cartItems} />
+        </div>
+    );
+}
+
+export default Header;

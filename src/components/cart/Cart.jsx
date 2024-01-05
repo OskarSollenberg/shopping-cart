25199e93d7317952a085bc1f20b0ea53d21eda37
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-01-05 20:36:20 +0100

    .

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -7,30 +7,36 @@ export default function Cart(props) {
     const { cartItems } = props;
 
     function handleOpenCart() {
-        setIsCartOpen(!isCartOpen);
-    }
-
-    function handleCloseCart() {
-        setIsCartOpen(false);
+        {
+            isCartOpen ? setIsCartOpen(false) : setIsCartOpen(true);
+        }
     }
 
     return (
-        <div className="cart">
-            <img
-                onMouseEnter={handleOpenCart}
-                className="cart__img"
-                src={cartImage.url}
-                alt=""
-            />
-            {cartItems > 0 && <p className="cart__counter">{cartItems} </p>}
+        <>
+            <div className="cart">
+                <img
+                    onClick={handleOpenCart}
+                    className={`cart__img ${isCartOpen && "cart__img--active"}`}
+                    src={cartImage.url}
+                    alt=""
+                />
+                <p
+                    className={`cart__counter ${
+                        cartItems > 0 && "cart__counter--active"
+                    }`}
+                >
+                    {cartItems}{" "}
+                </p>
+            </div>
 
-            <div className="cart__list">
+            <div className={`cart__list ${isCartOpen && "cart__list--active"}`}>
                 <p>Cart Item</p>
                 <p>Cart Item</p>
                 <p>Cart Item</p>
                 <p>Cart Item</p>
                 <p>Cart Item</p>
             </div>
-        </div>
+        </>
     );
 }

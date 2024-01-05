1d291dd9391134e8a2b4fb6c8af10a57b220cfa5
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-01-05 16:52:51 +0100

    .

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,13 +1,29 @@
 import "./cartStyles.css";
 import { cartImage } from "../../data";
+import { useState } from "react";
 
 export default function Cart(props) {
+    const [isCartOpen, setIsCartOpen] = useState(false);
     const { cartItems } = props;
+
+    function handleOpenCart() {
+        setIsCartOpen(!isCartOpen);
+    }
+
+    function handleCloseCart() {
+        setIsCartOpen(false);
+    }
 
     return (
         <div className="cart">
-            <img className="cart__img" src={cartImage.url} alt="" />
+            <img
+                onMouseEnter={handleOpenCart}
+                className="cart__img"
+                src={cartImage.url}
+                alt=""
+            />
             {cartItems > 0 && <p className="cart__counter">{cartItems} </p>}
+
             <div className="cart__list">
                 <p>Cart Item</p>
                 <p>Cart Item</p>
